ff9cc765b5872a4ce75b5d7849545284bca784c6
dhairyaj <dhairyaj@users.noreply.github.com>
2017-12-16 21:16:37 +0530

    dp page
    page was created to upload profile pic
    the actions related to the buttons are yet to be specified

---
+++ 2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload Image</title>
+	<style type="text/css">
+		#navbar{
+		width: 100%;
+		height: 50px;
+		background-color: #333;
+		font-size: 40px;
+		color: white;
+	}
+
+		img{
+			height: 200px;
+			width: 200px;
+			border: 2px solid black;
+			display: inline-block;
+		}
+	</style>
+</head>
+<body>
+
+<div id="navbar">
+	INTERCONNECT</div>
+<div style="text-align: center;">
+	<br><br><br><br>
+	<img  src="http://www.marismith.com/wp-content/uploads/2014/07/facebook-profile-blank-face.jpeg">
+	<br><br>
+	<h3>Upload an Image</h3>
+	<button type="button" style="margin-right: 25px;font-size: 15px;">Choose</button>
+	<button type="button" style="font-size: 15px;">Upload</button>
+</div>
+</body>
+</html>
