872ea57df791e2fa106f3520fb6c59dc089619e4
frankhart2017 <sdharchou@gmail.com>
2017-12-18 21:29:03 +0530

    Resume upload added

---
+++ resumeUpload.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width">
+        
+        <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+        <script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+
+        <title>resume upload</title>
+        <style type="text/css">
+
+            .navbar-custom {
+                background: #19D1FF;
+            }
+            .navbar-brand {
+                color: white;
+                font-weight: bold;
+                font-size: 150%;
+            }
+             #upload {
+                background: #19D1FF;
+                border: 2px solid white;
+                padding: 5px 10px;
+                color: white;
+                text-transform: uppercase;
+                font-weight: bold; 
+                border-radius: 10px;
+            }
+            
+        </style>
+    </head>
+    <body>
+
+        <nav class="navbar navbar-toggleable-md navbar-inverse navbar-custom">
+          <a class="navbar-brand" href="#">INTERCONNECT</a>
+        </nav>
+        
+        <div style="text-align: center;">
+            <br>
+            <h1 style="text-align: center;">Upload Resume</h1><br>
+            <input type="file"><br><br>
+            <button style="font-size: 15px;" id="upload">Upload</button>
+        </div>
+        
+    </body>
+</html>
